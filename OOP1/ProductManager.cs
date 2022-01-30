9554c0551edf43d8abf19261c343bb4d0696d248
Can <mccankaya45@hotmail.com>
2022-01-30 22:53:21 +0300

    Kodlama.io C#

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //Encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine("Ürün Eklendi : "+product.ProductName);
+        }
+
+        public void Delete(Product product)
+        {
+            Console.WriteLine("Ürün Silindi : "+product.ProductName);
+        }
+    }
+}
