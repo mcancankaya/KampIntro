384547b40cd7ea6025835a68c3d066b0f61ca09c
Can <mccankaya45@hotmail.com>
2022-02-04 02:50:37 +0300

    Kodlama.io C# 5.Gün Ders Sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //İş sınıfları // Business Classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            
+            Console.WriteLine("Müşteri Eklendi : "+musteri.MusteriNo);
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();        
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { new SmsLoggerService(),new DatabaseLoggerService() };
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), loggers);
+
+            List<IKrediManager> kredis = new List<IKrediManager> { ihtiyacKrediManager,konutKrediManager,tasitKrediManager};
+            //basvuruManager.KrediOnBilgilendirmesiYap(kredis);
+
+
+        }
+    }
+}
